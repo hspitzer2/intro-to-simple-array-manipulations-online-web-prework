1d8ca54c6953462d989e2e52e9d2e8e7a8ad7870
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-08 19:42:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,8 +18,7 @@ def using_pop(array)
 
 def pop_with_args(array)
   @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-  @chars_arya_killed = pop_with_args(@chars_in_game_of_thrones).pop(2)
-  @chars_arya_killed
+  @chars_arya_killed = pop_with_args(@chars_in_game_of_thrones).pop.last
 end
 
 
