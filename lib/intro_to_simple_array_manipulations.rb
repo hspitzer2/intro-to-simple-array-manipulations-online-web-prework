92f25188736887942324a90a9a2135c1c48eb8b5
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-08 20:03:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,23 +1,23 @@
 def using_push(array1, array2)
-  Array.push
+  array.push
 end
 
 def using_unshift(array1, array2)
-  Array.unshift
+  array.unshift
 end
 
 def using_pop(array)
-  Array.pop
+  array.pop
   end
 
 def pop_with_args(array)
-  Array.pop(2)
+  array.pop(2)
 end
 
 def using_shift(array)
-  Array.shift
+  array.shift
 end
 
 def shift_with_args(array)
-  Array.shift(2)
+  array.shift(2)
 end
