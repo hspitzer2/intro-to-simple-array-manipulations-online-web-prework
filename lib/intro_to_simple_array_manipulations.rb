942597efb7de625fee8e24a42192999434aacbff
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-08 20:19:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,9 @@
 def using_push(array, string)
   array.last(string)
-end 
+end
 
-def using_unshift(array1, string)
-  array.unshift
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
 def using_pop(array)
